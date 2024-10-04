e46bbfcec11110e6cd87f2aeeba2ed4dcb7d8733
pyyupsk <pyyupsk@proton.me>
2024-10-04 21:22:28 +0700

    chore(assembly): update assembly attributes
    Refines assembly metadata with a detailed description, company, product name, and updated copyright year.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,12 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("launch")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("launch")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("A modified version of VRChat's launch.exe designed to enhance the launching experience for VRChat by streamlining the process and improving performance while prioritizing user privacy.")]
+[assembly: AssemblyCompany("VRChat Launcher Optimization Project")]
+[assembly: AssemblyProduct("VRChat Launcher Optimization")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: ComVisible(false)]
 [assembly: Guid("da56ea75-245e-4df4-aa08-0b4389fa1ce6")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
